9d26359955fe88014c485a0ad569d1f5d989f59a
Renato Pessoa <renatopessoaneto@gmail.com>
2020-05-07 17:59:04 -0300

    modificações no index e script

--- scripts.js
+++ scripts.js
@@ -1,49 +1,35 @@
+const endereco = 'https://api.sheety.co/30b6e400-9023-4a15-8e6c-16aa4e3b1e72';
+
+async function getDados(url){
+  const response = await fetch(url);
+  return await response.json();
+}
+
+function createCard(element) {
+  document.getElementById('colunasCards').innerHTML += `
+      <div class="col">
+        <div class="card" style="width: 18rem;">
+          <img class="card-img-top" src="${element.photo}" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">${element.property_type}</h5>
+            <p class="card-text">${element.name} <br>R$: ${element.price},00</p>
+        </div>
+      </div>
+    </div>
+    `
+  }
+
+  function popular(dados) {
+    dados.forEach(element => {
+      createCard(element);
+    })
+  }
 
 window.onload = async () => {
-  const resposta = await fetch('https://api.sheety.co/30b6e400-9023-4a15-8e6c-16aa4e3b1e72')
 
-  const dados = await resposta.json();
+  const dados = await getDados(endereco);
 
-  let names = await dados.map(dados => `<li>${dados.name}</li>`)
-  console.log(names)
-
-  let nomes = names.toString().replace(/\,/g, "");
-  console.log(nomes);
-
-  dados.forEach(element => {
-    console.log(element);
-    let number = 1;
-    /*
-    if(number == 4) {
-      document.getElementById('colunas').innerHTML += `<div class="w-100"></div>`;
-      number = 1;
-    } else {
-      document.getElementById('colunas').innerHTML += `
-      <div class="col">
-        <div class="card" style="width: 18rem;">
-          <img class="card-img-top" src="${element.photo}" alt="Card image cap">
-          <div class="card-body">
-            <h5 class="card-title">${element.property_type}</h5>
-            <p class="card-text">${element.name}</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-      </div>
-    </div>
-    `
-    number++;
-    }*/
-    document.getElementById('colunas').innerHTML += `
-      <div class="col">
-        <div class="card" style="width: 18rem;">
-          <img class="card-img-top" src="${element.photo}" alt="Card image cap">
-          <div class="card-body">
-            <h5 class="card-title">${element.property_type}</h5>
-            <p class="card-text">${element.name} <br>Valor da diária: ${element.price}</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-      </div>
-    </div>
-    `
-  });
+  popular(dados);
   
 }
+
